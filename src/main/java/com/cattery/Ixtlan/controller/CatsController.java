be50f7e424f571b7fbe060117eed15375c32eaf7
Alex Trask <Sarlack_Work@mail.ru>
2020-03-16 13:23:35 +0300

    binding with postgres is done

--- src/main/java/com/cattery/Ixtlan/controller/CatsController.java
+++ src/main/java/com/cattery/Ixtlan/controller/CatsController.java
@@ -1,14 +1,35 @@
 package com.cattery.Ixtlan.controller;
 
+import com.cattery.Ixtlan.domain.Adults;
+import com.cattery.Ixtlan.repo.AdultsRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("cats")
 public class CatsController {
-  @GetMapping
-  public String Cats() {
-    return "cats";
+
+  private final AdultsRepo adultsRepo;
+
+  @Autowired
+  public CatsController(AdultsRepo adultsRepo) {
+    this.adultsRepo = adultsRepo;
   }
+
+  @GetMapping
+  public List<Adults> Cats() {
+    return adultsRepo.findAll();
+  }
+
+  @PostMapping
+  public Adults create(@RequestBody Adults adult) {
+    return adultsRepo.save(adult);
+  }
+
 }
---
+++ src/main/java/com/cattery/Ixtlan/repo/AdultsRepo.java
@@ -0,0 +1,8 @@
+package com.cattery.Ixtlan.repo;
+
+import com.cattery.Ixtlan.domain.Adults;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdultsRepo extends JpaRepository<Adults, Long> {
+
+}
